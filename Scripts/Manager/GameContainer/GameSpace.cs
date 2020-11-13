839d66aab84de36532e37fc8379277bd5d529da0
Vladimir Kharitonov <Volodia20.ru@gmail.com>
2020-11-13 11:47:34 +0300

    Commit

---
+++ Scripts/Manager/GameContainer/GameSpace.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.GameSpace.Advertisements;
+
+namespace Orchard.GameSpace
+{
+    public static class GameManager
+    {
+        public static GameInfo GameInfo => GameContainer.Instance.GameInfo;
+        public static GameConfig Config => GameContainer.Instance.GameConfig;
+        public static LevelLoadingData LevelLoadingData => GameContainer.Instance.LevelLoadingData;
+        public static GeneralAudio Audio => GameContainer.Instance.GeneralAudio;
+        public static AdsController Ads => GameContainer.Instance.AdsController;
+        public static Localization Localization => GameContainer.Instance.Localization;
+        public static GameNotification Notification => GameContainer.Instance.Notification;
+    }
+}

402b120953cf0aa81e17a40574875485e8985a60
Vladimir Kharitonov <Volodia20.ru@gmail.com>
2020-11-13 12:19:03 +0300

    Commit

---
+++ Scripts/Tools/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    _instance = singleton.AddComponent<T>();
+                    singleton.name = $"[SINGLETON] {typeof(T)}";
+                }
+
+                _instance.gameObject.transform.SetParent(null, false);
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = FindObjectOfType<T>();
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        MonoBehaviour[] scripts = Object.FindObjectsOfType<MonoBehaviour>();
+        foreach (MonoBehaviour script in scripts)
+        {
+            script.enabled = false;
+        }
+    }
+}
